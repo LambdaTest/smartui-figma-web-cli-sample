ed151ab07ac98a2037da598c37efe377d6e8bcde
Jeevesh <109514265+JeeveshJ7@users.noreply.github.com>
2025-01-09 15:18:05 +0530

    Update figma-web-local.js

--- figma-web-local.js
+++ figma-web-local.js
@@ -5,8 +5,6 @@ const { smartuiSnapshot } = require("@lambdatest/selenium-driver");
   let driver = await new Builder().forBrowser("chrome").build();
 
   try {
-    // await driver.get("https://www.lambdatest.com/");
-    // await smartuiSnapshot(driver, "LambdaTest-Figma.png");
 
     // Viewport: 1728x2227 px
     let ss1options =
@@ -92,52 +90,6 @@ const { smartuiSnapshot } = require("@lambdatest/selenium-driver");
     await driver.get("http://avexaero.com/services/handling/");
     await new Promise(r => setTimeout(r, 15000));
     await smartuiSnapshot(driver, "Handling.png",ss5options);
-
-    //     // vaneck
-        // await driver.get("https://www.vaneck.com/us/en/");
-        // await smartuiSnapshot(driver, `"{~!@#$%^&*(,./<>?:';;'[""""]\|)_+=-":;;'"÷`);
-
-    // await driver.get("https://www.vaneck.com/us/en/investments/durable-high-dividend-etf-dura/performance/");
-    // await smartuiSnapshot(driver, "screenshot-performance");
-
-    // await driver.get("https://www.vaneck.com/us/en/investments/biotech-etf-bbh/overview/");
-    // await smartuiSnapshot(driver, "screenshot-overview");
-
-    // await driver.get("https://www.vaneck.com/us/en/investments/energy-income-etf-einc/overview/");
-    // await smartuiSnapshot(driver, "energy-income-etf-einc");
-
-    // await driver.get("https://www.vaneck.com/us/en/investments/brazil-small-cap-etf-brf/overview/");
-    // await smartuiSnapshot(driver, "brazil-small-cap-etf-brf");
-
-    // await driver.get("https://www.vaneck.com/us/en/resources/tax-information/");
-    // await smartuiSnapshot(driver, "screenshot-tax-information");
-
-    // await driver.get("https://www.vaneck.com/us/en/education/education-center/");
-    // await smartuiSnapshot(driver, "screenshot-education-center");
-
-    // await driver.get("https://www.vaneck.com/us/en/education/investment-ideas/income-ideas/");
-    // await smartuiSnapshot(driver, "screenshot-income-ideas");
-
-    // await driver.get("https://www.vaneck.com/us/en/insights/digital-assets/");
-    // await smartuiSnapshot(driver, "screenshot-digital-assets");
-
-    // await driver.get("https://www.vaneck.com/us/en/insights/model-portfolios/?p=1");
-    // await smartuiSnapshot(driver, "{~!@#$%^&*(,./<>?:';;'[12]\|)_+=-`:");
-
-    // await driver.get("https://www.vaneck.com/us/en/insights/topics/?tag=BITCOIN&p=1");
-    // await smartuiSnapshot(driver, "{~!@#$%^&*(,./<>?:';;'[abc]\|)_+=-`:");
-
-    // await driver.get("https://www.vaneck.com/us/en/news-and-insights/thought-leaders/imaru-casanova/");
-    // await smartuiSnapshot(driver, "screenshot-imaru-casanova");
-
-    // await driver.get("https://www.vaneck.com/us/en/our-firm/leadership/");
-    // await smartuiSnapshot(driver, "screenshot-leadership");
-
-    // await driver.get("https://www.vaneck.com/us/en/our-firm/vaneck-apparel/");
-    // await smartuiSnapshot(driver, "screenshot-vaneck-apparel");
-
-    // await driver.get("https://www.vaneck.com/us/en/insights/investment-outlook/?p=1");
-    // await smartuiSnapshot(driver, "screenshot-investment-outlook");
 
   } finally {
     await driver.quit();
